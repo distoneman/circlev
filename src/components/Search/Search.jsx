98bb13979c06f1b3dcb65abae09451203cef6642
distoneman <distoneman@gmail.com>
2019-04-30 13:44:57 -0600

    search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -43,7 +43,7 @@ export default class Search extends Component {
         }
     }
 
-     searchOne = async (invoiceID) => {
+    searchOne = async (invoiceID) => {
         // console.log(invoiceID)
         const res = await axios.get(`/search/${this.state.searchType}ID/${invoiceID}`)
         // console.log(res.data)
@@ -54,13 +54,13 @@ export default class Search extends Component {
 
     }
 
-     printInvoice = async (invoiceID) => {
+    printInvoice = async (invoiceID) => {
         // console.log(invoiceID)
         await this.searchOne(invoiceID)
         const doc = new jsPDF({
-          orientation: 'p',
-          unit: 'mm',
-          format: [396, 612]  //5.5in by 8.5in paper
+            orientation: 'p',
+            unit: 'mm',
+            format: [396, 612]  //5.5in by 8.5in paper
         });
         doc.setFontSize(11);
         let slaughterTotal = (this.state.invoice[0].total_slaughter).toLocaleString('us-US', { style: 'currency', currency: 'USD' });
@@ -76,83 +76,108 @@ export default class Search extends Component {
         doc.text(this.state.invoice[0].customer, 15, 45);
         doc.text(this.state.invoice[0].phone, 15, 55);
         doc.text(`${this.state.invoice[0].baskets} Basket - Row ${this.state.invoice[0].row_num}`, 20, 63);
-        if(this.state.invoice[0].slaughter !== 0) {
-          doc.text(this.state.invoice[0].slaughter, 20, 85, {align: 'right'});
-          doc.text('Beef Slaughter', 27, 85);
-          doc.text(`$${this.state.invoice[0].price_slaughter}`, 102, 85, {align: 'right'});
-          doc.text(slaughterTotal, 132, 85, {align: 'right'})
-        }  
-        if(this.state.invoice[0].qtyCut !== 0) {
-          doc.text(this.state.invoice[0].qty_cut, 20, 95, {align: 'right'})
-          doc.text('Cut & Wrap', 27, 95);
-          doc.text(`$${this.state.invoice[0].price_cut}`, 102, 95, {align: 'right'});
-          doc.text(cutWrapTotal, 132, 95, {align: 'right'});
+        if (this.state.invoice[0].slaughter !== 0) {
+            doc.text(this.state.invoice[0].slaughter, 20, 85, { align: 'right' });
+            doc.text('Beef Slaughter', 27, 85);
+            doc.text(`$${this.state.invoice[0].price_slaughter}`, 102, 85, { align: 'right' });
+            doc.text(slaughterTotal, 132, 85, { align: 'right' })
         }
-        if(this.state.invoice[0].qty_patties !== 0){
-          doc.text(this.state.invoice[0].qty_patties, 20, 105, {align: 'right'})
-          doc.text('Patties', 27, 105);
-          doc.text(`$${this.state.invoice[0].price_patties}`, 102, 105, {align: 'right'});
-          doc.text(pattiesTotal, 132, 105, {align: 'right'});
+        if (this.state.invoice[0].qtyCut !== 0) {
+            doc.text(this.state.invoice[0].qty_cut, 20, 95, { align: 'right' })
+            doc.text('Cut & Wrap', 27, 95);
+            doc.text(`$${this.state.invoice[0].price_cut}`, 102, 95, { align: 'right' });
+            doc.text(cutWrapTotal, 132, 95, { align: 'right' });
         }
-        if(this.state.invoice[0].qty_brand !== 0){
-          doc.text(this.state.invoice[0].qty_brand, 20, 115, {align: 'right'});
-          doc.text('Brand Inspection', 27, 115);
-          doc.text(`$${this.state.invoice[0].price_brand}`, 102, 115, {align: 'right'});
-          doc.text(brandTotal, 132, 115, {align: 'right'});
+        if (this.state.invoice[0].qty_patties !== 0) {
+            doc.text(this.state.invoice[0].qty_patties, 20, 105, { align: 'right' })
+            doc.text('Patties', 27, 105);
+            doc.text(`$${this.state.invoice[0].price_patties}`, 102, 105, { align: 'right' });
+            doc.text(pattiesTotal, 132, 105, { align: 'right' });
         }
-        if(Number(this.state.invoice[0].qty_other) !== 0 ){
-          doc.text(this.state.invoice[0].qty_other, 20, 125, {align: 'right'});
-          doc.text(this.state.invoice[0].desc_other, 27, 125);
-          doc.text(`$${this.state.invoice[0].price_other}`, 102, 125, {align: 'right'});
-          doc.text(otherTotal, 132, 125, {align: 'right'});
+        if (this.state.invoice[0].qty_brand !== 0) {
+            doc.text(this.state.invoice[0].qty_brand, 20, 115, { align: 'right' });
+            doc.text('Brand Inspection', 27, 115);
+            doc.text(`$${this.state.invoice[0].price_brand}`, 102, 115, { align: 'right' });
+            doc.text(brandTotal, 132, 115, { align: 'right' });
+        }
+        if (Number(this.state.invoice[0].qty_other) !== 0) {
+            doc.text(this.state.invoice[0].qty_other, 20, 125, { align: 'right' });
+            doc.text(this.state.invoice[0].desc_other, 27, 125);
+            doc.text(`$${this.state.invoice[0].price_other}`, 102, 125, { align: 'right' });
+            doc.text(otherTotal, 132, 125, { align: 'right' });
         }
         doc.text('Total', 100, 135);
-        doc.text(total, 132, 135, {align: 'right'});
-        if(this.state.invoice[0].message != null){
-            doc.text(this.state.invoice[0].message, 27, 150, {maxWidth: '90'})
+        doc.text(total, 132, 135, { align: 'right' });
+        if (this.state.invoice[0].message != null) {
+            doc.text(this.state.invoice[0].message, 27, 150, { maxWidth: '90' })
         }
         doc.text(`${this.state.invoice[0].net_weight} Net Weight Misc. Beef Cuts`, 27, 200)
         doc.save('invoice.pdf')
-        // doc.autoPrint({});
-        // var iframe = document.getElementById('output');
-        // iframe.src = doc.output('dataurlstring');
-      }
+        doc.autoPrint({});
+        var iframe = document.getElementById('output');
+        iframe.src = doc.output('dataurlstring');
+    }
 
     render() {
         let searchResults = this.state.searchResults.map(invoice => {
+            let iDate = moment(invoice.invoice_date).format('MM/DD/YYYY')
             return (
 
                 <SearchDisplay
                     key={invoice.beef_id}
                     id={invoice.beef_id}
+                    iDate={iDate}
                     customer={invoice.customer}
+                    soldBy={invoice.sold_by}
+                    phone={invoice.phone}
+                    total={invoice.total}
+                    weight={invoice.net_weight}
                     printInvoice={this.printInvoice}
                 />
             )
         })
 
         return (
-            <div className='search-main'>Search
-                <hr />
-                <select name="search-type" id="search-type"
-                    onChange={(e) => this.handleChange("searchType", e)}>
-                    <option value="beef">Beef</option>
-                    <option value="pork">Pork</option>
-                    <option value="sheep">Sheep</option>
-                    <option value="other">Other</option>
-                </select>
-                <select name="search-field" id="search-field"
-                    onChange={e => this.handleChange("searchField", e)}>
-                    <option value="customer">Customer</option>
-                    <option value="soldBy">Sold By</option>
-                </select>
-                <input type="text" className="user-input"
-                    onChange={e => this.handleChange("searchCriteria", e)} />
-                <button className='search-btn'
-                    onClick={() => this.search()}>Search</button>
-                <div>
-                    {searchResults}
+            <div className='search-main'>
+                <div className='search-container'>
+                    <span className='search-title'>Search</span>
+                    <hr />
+                    <div className='search-form'>
+                        <label className='search-label'>Search Type:</label>
+                        <select name="search-type" id="search-type"
+                            className='search-select'
+                            onChange={(e) => this.handleChange("searchType", e)}>
+                            <option value="beef">Beef</option>
+                            <option value="pork">Pork</option>
+                            <option value="sheep">Sheep</option>
+                            <option value="other">Other</option>
+                        </select>
+                        <label className='search-label'>Search Field:</label>
+                        <select name="search-field" id="search-field"
+                            className='search-select'
+                            onChange={e => this.handleChange("searchField", e)}>
+                            <option value="customer">Customer</option>
+                            <option value="soldBy">Sold By</option>
+                        </select>
+                        <label className='search-label'>Search Criteria:</label>
+                        <input type="text" className="search-user-input"
+                            onChange={e => this.handleChange("searchCriteria", e)} />
+                        <div></div>
+                        <button className='search-button'
+                            onClick={() => this.search()}>Search</button>
+                    </div>
                 </div>
+                <div className='search-results-title'>
+                    <div className='search-results-item-title'>Invoice Date</div>
+                    <div className='search-results-item-title'>Customer Name</div>
+                    <div className='search-results-item-title'>Sold By</div>
+                    <div className='search-results-item-title'>Phone</div>
+                    <div className='search-results-item-title'>Total</div>
+                    <div className='search-results-item-title'>Weight</div>
+                    <div className='search-results-item-title'></div>
+                </div>
+                    {searchResults}
+                    <iframe title="pdf" id="output" className='beef-pdf-iframe'></iframe>
             </div>
         )
     }
--- src/components/Search/SearchDisplay.jsx
+++ src/components/Search/SearchDisplay.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Search.css';
+import { FaPrint, FaTrashAlt, FaEdit, FaSearch, FaEnvelope, FaComment } from 'react-icons/fa';
+import { IoIosMail, IoIosText } from "react-icons/io";
+
 
 export default function SearchDisplay(props){
     return(
-        <div>
-        {props.customer}
-        <button onClick={() => props.printInvoice(props.id)}>Print</button>
+        <div className='search-results-container'>
+            <div className='search-item'>{props.iDate}</div>
+            <div className='search-item'>{props.customer}</div>
+            <div className='search-item'>{props.soldBy}</div>
+            <div className='search-item'>{props.phone}</div>
+            <div className='search-item'>${props.total}</div>
+            <div className='search-item'>{props.weight}</div>
+            <div className='search-item'>
+                <FaPrint className='fa-icon'
+                    onClick={() => props.printInvoice(props.id)}/>
+                <FaSearch className='fa-icon' />
+                <FaEdit className='fa-icon' />
+                <FaTrashAlt className='fa-icon'/>
+                <FaEnvelope className='fa-icon' />
+                <FaComment className='fa-icon' />
+            </div>
         </div>
 
     )
